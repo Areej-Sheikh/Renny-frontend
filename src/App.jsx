f6fa1440455d85c21a82d37e73d12b33ad054df5
nooreinfatima <nooreinfatima43@gmail.com>
2026-01-01 12:16:02 +0000

    Update career, job details,and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ import Events from './pages/Media/Events';
 
 /* Career */
 import Career from './pages/Career/Career';
+import JobDetails from "./pages/Career/JobDetails";
 
 /* Contact */
 import Contact from './pages/Contact/Contact';
@@ -192,10 +193,11 @@ const App = () => {
         />
 
         {/* Career */}
-        <Route path="/careers/" element={<Career />} />
+        <Route path="/career/" element={<Career />} />
 
         {/* Contact */}
-        <Route path="/contact/" element={<Contact />} />
+        <Route path="/contact-us/" element={<Contact />} />
+        <Route path='/careers/:jobId' element={<JobDetails/>}/>
       </Routes>
       <Parallax />
       <Footer />

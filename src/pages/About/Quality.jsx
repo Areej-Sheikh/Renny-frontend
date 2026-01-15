540129fd7e4181fa316e3b5c09f0a57c7734b9cd
nooreinfatima <nooreinfatima43@gmail.com>
2026-01-15 13:27:35 +0000

    Certificate #2

--- src/pages/About/Quality.jsx
+++ src/pages/About/Quality.jsx
@@ -31,6 +31,7 @@ import cert1 from "../../assets/Certificate-1.webp";
 import cert2 from "../../assets/Certificate-2.webp";
 import cert3 from "../../assets/Certificate-3.webp";
 import cert4 from "../../assets/Certificate-4.webp";
+import CertificateCard from "../../components/CertificateCard";
 
 const Quality = () => {
   const specificationsData = [
@@ -476,42 +477,12 @@ const Quality = () => {
                     gap-6 sm:gap-8 lg:gap-10"
           >
             {[
-              { img: cert1, title: "IS 1239-1:2004" },
-              { img: cert2, title: "IS 1161:2014" },
-              { img: cert3, title: "IS 3601:2006" },
-              { img: cert4, title: "IS 4923:2017" },
+              { img: cert1, title: "IS 1239-1:2004", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt itaque provident exercitationem libero aperiam iste ut maxime placeat velit recusandae." },
+              { img: cert2, title: "IS 1161:2014" , description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt itaque provident exercitationem libero aperiam iste ut maxime placeat velit recusandae."},
+              { img: cert3, title: "IS 3601:2006", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt itaque provident exercitationem libero aperiam iste ut maxime placeat velit recusandae." },
+              { img: cert4, title: "IS 4923:2017" , description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt itaque provident exercitationem libero aperiam iste ut maxime placeat velit recusandae."},
             ].map((item, index) => (
-              <motion.a
-                key={index}
-                href={item.img}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="block bg-white rounded-xl shadow-md overflow-hidden
-                     cursor-pointer"
-                whileHover={{ scale: 1.06 }}
-                whileTap={{ scale: 0.98 }}
-                transition={{ type: "spring", stiffness: 220, damping: 18 }}
-              >
-                {/* Certificate Image */}
-                <div className="p-4">
-                  <img
-                    src={item.img}
-                    alt={item.title}
-                    className="w-full h-auto rounded-lg border"
-                  />
-                </div>
-
-                {/* Footer */}
-                <div
-                  className="bg-[#292c44] text-white
-                          flex items-center justify-between
-                          px-4 py-3"
-                >
-                  <span className="text-sm font-semibold">{item.title}</span>
-
-                  <span className="text-xs font-medium opacity-80">View</span>
-                </div>
-              </motion.a>
+              <CertificateCard key={item.id} certificateDetails={item}/>
             ))}
           </div>
         </div>
